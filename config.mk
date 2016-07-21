359468f794605274ccb0d82e39868787185cf868
Eric Hegnes <eric.hegnes@gmail.com>
2016-07-20 20:33:35 -0400

    Add optimizations and link against proper X11 libs
    Optimizations include:
    - LTO
    - Native CPU opts
    - Graphite opts
    - Linking using `gold`

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 6.1
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -24,11 +24,19 @@ FREETYPEINC = /usr/include/freetype2
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
+# optimizations
+LTOFLAGS      = -flto=jobserver -fuse-linker-plugin -fno-trapping-math -fno-signed-zeros
+GRAPHITEFLAGS = -ftree-vectorize -floop-interchange -floop-strip-mine -floop-block
+RELEASEFLAGS  = -march=native -mtune=native -Ofast -pipe -g0 -funroll-loops
+
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-LDFLAGS  = -s ${LIBS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS} ${LTOFLAGS} ${GRAPHITEFLAGS} ${RELEASEFLAGS}
+LDFLAGS  = -s ${LIBS} ${CFLAGS}
+
+# use gold linker
+LDFLAGS += -fuse-ld=gold
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
